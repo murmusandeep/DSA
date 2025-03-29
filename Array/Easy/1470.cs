d80e73d44f7cdb304e7e814c8166ca22ee64a4d5
Sandeep Murmu <murmusandeep17@gmail.com>
2025-03-29 10:21:27 +0530

    shuffle array

---
+++ Array/Easy/1470.cs
@@ -0,0 +1,30 @@
+﻿namespace Array.Easy
+{
+    /// <summary>
+    /// Shuffle the Array
+    /// </summary>
+    /// More details: <see href="https://leetcode.com/problems/shuffle-the-array/description/"/>
+    /// <example>
+    /// <para>Example usage:</para>
+    /// <code>
+    /// int[] nums = [2,5,1,3,4,7], n = 3
+    /// int result = Shuffle(nums, n)
+    /// Output: [2,3,5,4,1,7] 
+    /// </code>
+    /// </example>
+    public static class _1470
+    {
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[2 * n];
+
+            for (int i = 0, j = 0; i < n; i++, j += 2)
+            {
+                result[j] = nums[i];
+                result[j + 1] = nums[i + n];
+            }
+
+            return result;
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -6,9 +6,10 @@ namespace Array.Program
     {
         public static void Main(string[] args)
         {
-            int[][] nums = [[1, 5], [7, 3], [3, 5]];
-            int result = _1672.MaximumWealth(nums);
-            Console.WriteLine(result);
+            int[] nums = [2, 5, 1, 3, 4, 7];
+            int n = 3;
+            int[] result = _1470.Shuffle(nums, n);
+            Console.WriteLine(string.Join(",", result));
         }
     }
 }
